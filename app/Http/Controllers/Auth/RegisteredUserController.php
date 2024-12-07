4a93c7fb640571c8fc3babf76c3b6caf29d2df14
sonu <sonucjha@gmail.com>
2024-12-07 13:39:38 +0530

    'password' => ['required', 'string', 'min:3', 'confirmed'],

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,13 +32,15 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            // 'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'string', 'min:3', 'confirmed'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            // 'password' => ['required', 'string', 'min:3', 'confirmed'],
         ]);
 
         event(new Registered($user));
